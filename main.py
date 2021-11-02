fbb30e740e26e86c6588ba9bec7fdaa337cb9936
alihantutuk <tutuk4354@gmail.com>
2021-11-02 16:45:25 +0300

    write main structure for special purposes

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import cv2
+from skimage import io
+import matplotlib.pyplot as plt
+import glob
+import os
+from augmentation import Augmentor
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+all_images = glob.glob("ornekler/*.jpg")
+transform_type = {1: "soft", 2: "medium", 3: "hard"}
+image_per_type = 5
+for img_path in all_images:
+    file_name = img_path.rsplit(os.sep, 1)[1].split(".")[0]
+    img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+    augmentor = Augmentor(img)
+    for j_type in range(1, 4):
+        augmentor.type = transform_type.get(j_type, "soft")
+        for i in range(image_per_type):
+            dest_file_name = f"{file_name}_{i + 1}_{augmentor.type}.jpg"
+            augmented = augmentor.transform_image()
+            new_image = augmented["image"]
+            cv2.imwrite(f"augmented_images{os.sep}{dest_file_name}", new_image)
+    break
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
